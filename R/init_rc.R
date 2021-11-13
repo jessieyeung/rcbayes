41b7be76e82edec0fb568056760efe519f11cb24
Jessie Yeung <jessieyeung1@gmail.com>
2021-11-13 13:29:41 -0500

    Allow option for mx as input

--- R/init_rc.R
+++ R/init_rc.R
@@ -7,6 +7,7 @@
 #' @param ages numeric. A vector of integers for ages.
 #' @param migrants numeric. A vector of integers for observed age-specific migrants.
 #' @param pop numeric. A vector of integers for age-specific population.
+#' @param mx numeric. A vector of age-specific migration rates.
 #' @param pre_working_age logical (TRUE/FALSE). Whether or not you are including pre working age component.
 #' @param working_age logical (TRUE/FALSE). Whether or not you are including working age component.
 #' @param retirement logical (TRUE/FALSE). Whether or not you are including retirement age component.
@@ -19,8 +20,8 @@
 #' @export
 #'
 #' @examples
-#' # define ages, migrants, and population
-#'ages <- 0:80
+#' # Ex. 1: Using ages, migrants, and population
+#' ages <- 0:80
 #'migrants <- c(202,215,167,188,206,189,164,
 #'             158,197,185,176,173,167,198,
 #'             203,237,249,274,319,345,487,
@@ -51,13 +52,46 @@
 #'         59556,59556,59556,59556,59556)
 #'
 #'#compute initial values
-#'iv <- init_rc(ages, migrants, pop, pre_working_age=TRUE,
-#'              working_age=TRUE, retirement=TRUE, post_retirement=TRUE)
+#'iv <- init_rc(ages=ages, migrants=migrants, pop=pop,
+#'              pre_working_age=TRUE,
+#'              working_age=TRUE,
+#'              retirement=TRUE,
+#'              post_retirement=TRUE)
 #'
-
+#' # Ex 2: Using ages and mx
+#' ages <- 0:80
+#' mx <- c(0.001914601, 0.002037818, 0.001582863, 0.001781906,
+#'         0.001952514, 0.001780902, 0.001545333, 0.001488796,
+#'         0.001856284, 0.001743211, 0.001758172, 0.001728203,
+#'         0.001668265, 0.001977943, 0.002027891, 0.002063022,
+#'         0.002167479, 0.002385097, 0.002776811, 0.003003134,
+#'         0.003558771, 0.003588001, 0.003807227, 0.003690307,
+#'         0.003865687, 0.003858488, 0.003814558, 0.003873131,
+#'         0.003712056, 0.003543659, 0.003290238, 0.003092965,
+#'         0.002811146, 0.002811146, 0.002677282, 0.002744282,
+#'         0.002311759, 0.002416161, 0.002155156, 0.002177528,
+#'         0.002064710, 0.002057062, 0.002179416, 0.001942356,
+#'         0.001873533, 0.001981783, 0.001921955, 0.001929434,
+#'         0.001966826, 0.001892041, 0.002244159, 0.001900401,
+#'         0.002153355, 0.002244159, 0.002263617, 0.002441776,
+#'         0.002655001, 0.002379872, 0.002366115, 0.002421141,
+#'         0.002621367, 0.002534252, 0.002431298, 0.002534252,
+#'         0.002455057, 0.002381964, 0.002345034, 0.002243477,
+#'         0.002363499, 0.002428126, 0.002292457, 0.002117078,
+#'         0.002154659, 0.002004334, 0.002079497, 0.001897374,
+#'         0.002216401, 0.001863792, 0.002182820, 0.001847001,
+#'         0.001897374)
+#'
+#' # compute initial values
+#' iv <- init_rc(ages=ages, mx=mx,
+#'              pre_working_age=TRUE,
+#'              working_age=TRUE,
+#'              retirement=TRUE,
+#'              post_retirement=TRUE)
 init_rc <- function(ages,
                     migrants,
                     pop,
+                    mx,
                     pre_working_age,
                     working_age,
                     retirement,
@@ -71,15 +105,27 @@ init_rc <- function(ages,
   if(!missing(net_mig)) stop("Argument net_mig deprecated, use migrants instead.")
 
   if(missing(ages)) stop("ages is missing")
-  if(missing(migrants)) stop("migrants is missing")
-  if(missing(pop)) stop("pop is missing")
 
-  if(length(ages)!=length(migrants) | length(migrants)!=length(pop))
-    stop("length of arguments ages, migrants and pop must be equal")
-  if ( !all(migrants == floor(migrants)) ) stop("migrants must comprise of integers")
-  if (sum(migrants > pop)>0) stop("must have migrants <= pop")
+  if(!missing(migrants) & !missing(pop)) {
 
-  rate <- migrants / pop
+    message("init_rc is using arguments ages, migrants, and pop")
+    if(length(ages)!=length(migrants) | length(migrants)!=length(pop))
+      stop("length of arguments ages, migrants and pop must be equal")
+    rate <- migrants / pop
+
+  } else if (!missing(mx)) {
+
+    message("init_rc is using arguments ages and mx")
+    if(length(ages)!=length(mx))
+      stop("length of arguments ages and mx must be equal")
+    rate <- mx
+
+  } else {
+
+    stop("init_rc requires either data for migrants and pop, or data for mx")
+
+  }
+
   rate_ranked <- order(rate, decreasing=T)
   rate_ranked_by_age <- ages[rate_ranked]
 
